da0a9d16b9dac71d5028fafadd180c2085545556
KarolDawidG <karoldawidg@gmail.com>
2023-06-17 01:05:59 +0200

    Log 11

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-// Dane wejściowe
+// Dane wejściowe dla Tab 1.
 const x = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 const y = [122, 125, 131, 135, 142, 145, 150, 154, 159, 164, 168];
 
-/// nowa funkcjonalnosc
-let y2 = [];
-
-const sendData = () => {
-  const dataTable = document.getElementById('data-table');
-  const rows = dataTable.getElementsByTagName('tr');
-  let y2 = [];
-  for (let i = 1; i < rows.length; i++) {
-    const yInput = document.getElementById(`yValue${i}`);
-    y2.push(parseInt(yInput.value));
-  }
-    wykres(x, y2, nazwaWykresuDwa);
-};
-
-
-////////////////////////////////////////////////////////////////////////
 // funkcja obliczana sredniej arytmetycznej
 const obliczSrednia = el =>{
     return el.reduce((a, b) => a + b, 0) / el.length;
@@ -39,11 +23,13 @@ const calculateA = (x, y, sredniaX, sredniaY) => {
 // obliczanie wartosci 'b'
 const calculateB = (wynikA, sredniaX, sredniaY) => {
     return sredniaY - sredniaX*wynikA;
-}
+};
+
 // szacowanie dowolnej wartosci 'x'
 const szacowanieDowolnegoX = (numer, wynikA, wynikB) =>{
     return numer*wynikA+wynikB;
-}
+};
+
 // sprawdzanie dowolnej wartosci od 1 do 30
 const szacowanie = () => {
     const numerInput = document.getElementById('wartoscInput');
@@ -65,48 +51,25 @@ document.getElementById('wynikAValueB').textContent = wynikB.toFixed(2);
 document.getElementById('wynikAValue19').textContent = wynik19.toFixed(2);
 document.getElementById('funkcja').textContent = `y = ${wynikA.toFixed(2)}x + ${wynikB.toFixed(2)}`;
 
-// wykres przedstawiający tab
+//wykresy
+//zmienne z nazwami wykresow
+const nazwaWykresuJeden = 'Wykres Tab 1.';
+const nazwaWykresuDwa = 'Wykres Tab 2.';
+const nazwaWykresuTrzy = 'Funkcja estymowana';
 
-const trace = {
-    x: x, // wartości osi X
-    y: y, // wartości osi Y
-    mode: 'lines',
-    line: {
-        color: 'blue'
-    }
-};
-
-const data = [trace];
-
-const layout = {
-    title: 'Zobrazowanie wartosci z Tab. 1',
-    width: 400,
-    height: 400,
-    xaxis: {
-        title: 'Masa ryby = X'
-    },
-    yaxis: {
-        title: 'Stężenie substancji chemicznej = Y'
-    }
-};
-
-Plotly.newPlot('myDiv', data, layout);
-
-/// drugie ///////////////////////////
-const nazwaWykresuDwa = 'myDivSecond';
-const wykres = (x, xxx, nazwaWykresuDwa) =>{
-    const trace2 = {
-        x: x, // wartości osi X
-        y: xxx, // wartości osi Y
+// funkcja rysujaca wykresy
+const wykres = (osX, osY, nazwaWykresu) =>{
+    const trace = {
+        x: osX, 
+        y: osY, 
         mode: 'lines',
         line: {
             color: 'red'
         }
     };
-
-    const data2 = [trace2];
-
-    const layout2 = {
+    const data = [trace];
+    const layout = {
+        title: nazwaWykresu,
         width: 400,
         height: 400,
         xaxis: {
@@ -116,17 +79,35 @@ const wykres = (x, xxx, nazwaWykresuDwa) =>{
             title: 'Stężenie substancji chemicznej = Y'
         }
     };
-
-    Plotly.newPlot(nazwaWykresuDwa, data2, layout2);
+    Plotly.newPlot(nazwaWykresu, data, layout);
 };
+// wywolanie wykresow
 
-//rysowanie wykresu funkcji estymowanej
-const nazwaWykresuTrzy = 'myDivThird';
+//wykres wartosci Tab 1.
+wykres(x, y, nazwaWykresuJeden)                          
+
+//wykres funkcji estymowanej
 const nowyX = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const nowyY = [];
 for (let numer = 1; numer <= nowyX.length; numer++) {
     nowyY.push(szacowanieDowolnegoX(numer, wynikA, wynikB).toFixed(2));
 }
-wykres(nowyX, nowyY, nazwaWykresuTrzy);
+wykres(nowyX, nowyY, nazwaWykresuTrzy);  
+
+/// wykres dla Tab 2.
+const sendData = () => {
+    const dataTable = document.getElementById('data-table');
+    const rows = dataTable.getElementsByTagName('tr');
+    let y2 = [];
+    for (let i = 1; i < rows.length; i++) {
+      const yInput = document.getElementById(`yValue${i}`);
+      y2.push(parseInt(yInput.value));
+    }
+      wykres(x, y2, nazwaWykresuDwa);                   
+  };
+
+
+
+
 
 
